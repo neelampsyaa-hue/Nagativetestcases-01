927c33c34ade77653a9d2b8773be3db77ce7e6ac
NeelamSyaa <neelam.P@syaa.in>
2026-03-27 17:42:16 +0530

    demo

---
+++ src/main/java/Nagativetestcases/Nagativetestcases_01/App.java
@@ -0,0 +1,10 @@
+package Nagativetestcases.Nagativetestcases_01;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/testdata/nagative.java
@@ -0,0 +1,9 @@
+package testdata;
+
+public class nagative {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+}
