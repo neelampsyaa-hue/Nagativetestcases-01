cab3687e31a5941cd2625dda544c8fe02c793322
NeelamSyaa <neelam.P@syaa.in>
2026-03-27 18:16:12 +0530

    demo test

--- src/test/java/testdata/nagative.java
+++ src/test/java/testdata/nagative.java
@@ -1,9 +1,23 @@
 package testdata;
 
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 public class nagative {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+	WebDriver driver = new  ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://pro.smlsaarthi.com/dashboard");
+
+	String Expectedreuslt = driver.getTitle();
+	System.out.println(Expectedreuslt);
+	        String  AchualResult  =  "SML SAARTHI";
+	        
+	     Assert.assertEquals(Expectedreuslt, AchualResult);
+	     driver.close();
 	}
 }
